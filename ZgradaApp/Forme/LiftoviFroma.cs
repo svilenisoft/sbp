23af9ad6469cb1bd18e530dc744ecc17b0769dea
milica aleksic <aleskicmilica@elfak.rs>
2022-05-28 15:17:22 +0200

    1

---
+++ ZgradaApp/Forme/LiftoviFroma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZgradaApp.Forme
+{
+    public partial class LiftoviFroma : Form
+    {
+        public LiftoviFroma()
+        {
+            InitializeComponent();
+        }
+    }
+}
